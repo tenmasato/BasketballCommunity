c2b01df02d2de8ac7866f874607a6ed12c32f92e
tenma <narito1015@icloud.com>
2021-12-08 04:21:50 +0000

    [Add]いいね

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :maps, only: [:index]
-  resources :tweets, only: [:new,:create,:index,:show,:destroy]
+  resources :tweets, only: [:new,:create,:index,:show,:destroy]do
+    resource :favorites,only: [:create,:destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
 end
