0dc5c97de9e2431bf9fab5725f8718cdf73d9f3d
tenma <narito1015@icloud.com>
2021-12-09 12:41:21 +0000

    [Add]フォロー・フォローワー

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,27 @@ class User < ApplicationRecord
   has_many :tweets,dependent: :destroy
   has_many :favorites,dependent: :destroy
   has_many :tweet_comments,dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+
   attachment :profile_image
+
+  #フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  #フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
 
   def name
     last_name + first_name
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_042459) do
+ActiveRecord::Schema.define(version: 2021_12_09_065402) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "followed_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
