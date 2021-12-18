d2d50605f53daf06394da5638db10a9d52e09bbe
tenma <narito1015@icloud.com>
2021-12-18 09:00:38 +0000

    [Add]フラッシュメッセージ

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -4,14 +4,15 @@ class CountsController < ApplicationController
   def create
     @map = Map.find(params[:map_id])
     count = @map.counts.new(user_id: current_user.id)
-    count.save
-    redirect_to request.referer
+    if count.save
+    redirect_to request.referer,notice: "コートにはいりました。バスケを楽しんで!!"
+    end
   end
 
   def destroy
     @map = Map.find(params[:map_id])
     count = @map.counts.find_by(user_id: current_user.id)
     count.destroy
-    redirect_to request.referer
+    redirect_to request.referer,notice: "コートをでました"
   end
 end
